71980c7aac2903beec1d042edf10111df26a6fb7
ghazalrs <ghazal990.r@gmail.com>
2026-02-28 17:00:33 -0500

    feat: add shadcn signup form

---
+++ frontend/src/app/(app)/layout.tsx
@@ -0,0 +1,19 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex flex-1 flex-col">
+        <header className="flex h-14 items-center border-b px-4">
+          <SidebarTrigger />
+        </header>
+        <TooltipProvider>
+          <div className="flex-1 p-6">{children}</div>
+        </TooltipProvider>
+      </main>
+    </SidebarProvider>
+  )
+}
