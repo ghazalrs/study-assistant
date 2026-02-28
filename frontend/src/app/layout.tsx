bbad01b249131bba7a7ff31c2c440e4cdf099abe
ghazalrs <ghazal990.r@gmail.com>
2026-02-28 12:44:13 -0500

    feat: implement AppSidebar component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,16 @@
-import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 import { TooltipProvider } from "@/components/ui/tooltip"
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <SidebarProvider>
+        <AppSidebar />
         <TooltipProvider>{children}</TooltipProvider>
+        </SidebarProvider>
       </body>
     </html>
-  );
+  )
 }
